a50d813c2cae3d6a9cda778cde1355c03ef7fd4a
rollrat <rollrat.cse@gmail.com>
2023-03-16 00:05:51 +0900

    feat(index): implements get with redirection

--- src/main.rs
+++ src/main.rs
@@ -150,6 +150,14 @@ impl Article {
             classes: self.classes(),
         }
     }
+
+    fn is_redirect(&self) -> bool {
+        self.text.starts_with("#redirect")
+    }
+
+    fn get_redirect(&self) -> &str {
+        &self.text[10..].trim()
+    }
 }
 
 #[allow(dead_code)]
@@ -212,7 +220,7 @@ impl TitleIndex {
         }
     }
 
-    fn get(&mut self, key: &str) -> Option<Article> {
+    fn get_no_redirect(&mut self, key: &str) -> Option<Article> {
         if !self.map.contains_key(key) {
             return None;
         }
@@ -232,6 +240,23 @@ impl TitleIndex {
         let raw = String::from_utf8(buf).unwrap();
 
         Some(serde_json::from_str(&raw).unwrap())
+    }
+
+    fn get(&mut self, key: &str) -> Option<Article> {
+        if !self.map.contains_key(key) {
+            return None;
+        }
+
+        match self.get_no_redirect(key) {
+            Some(x) => {
+                if x.is_redirect() {
+                    self.get_no_redirect(x.get_redirect())
+                } else {
+                    Some(x)
+                }
+            }
+            None => None,
+        }
     }
 }
 
