ada3157150f76e73d99ef7a2ba6d64f6a61097a2
rollrat <rollrat.cse@gmail.com>
2023-03-15 18:41:05 +0900

    feat(article): implements article structure

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 use regex::Regex;
 
@@ -29,24 +29,88 @@ fn split_json_file() {
     fs::write("2.json", &serde_json::to_string(&r3).unwrap()).unwrap();
 }
 
-fn get_classes(text: &str) -> Vec<&str> {
-    let mut result = Vec::new();
-    let re = Regex::new(r"\[\[분류:(.*?)\]\]").unwrap();
+#[allow(dead_code)]
+fn save_classes_rank() {
+    let js = load_dump();
 
-    for cap in re.captures_iter(text) {
-        result.push(cap.get(1).map_or("", |m| m.as_str()));
+    let mut hmap = HashMap::new();
+
+    for x in &js {
+        let class = x.classes();
+
+        for c in class {
+            *hmap.entry(c).or_insert(0) += 1;
+        }
     }
 
-    result
+    let mut v = hmap.into_iter().collect::<Vec<(&str, i32)>>();
+    v.sort_by(|a, b| a.1.cmp(&b.1));
+
+    fs::write("r.json", format!("{:#?}", v)).unwrap();
+}
+
+#[derive(Debug)]
+struct Article {
+    pub title: String,
+    text: String,
+}
+
+impl Article {
+    fn from(value: &serde_json::Value) -> Self {
+        Article {
+            // todo optimizing
+            // to_owned may be copying processing
+            title: value["title"].as_str().unwrap().to_owned(),
+            text: value["text"].as_str().unwrap().to_owned(),
+        }
+    }
+
+    fn classes(&self) -> Vec<&str> {
+        let mut result = Vec::new();
+        let re = Regex::new(r"\[\[분류:(.*?)\]\]").unwrap();
+
+        for cap in re.captures_iter(&self.text) {
+            result.push(cap.get(1).map_or("", |m| m.as_str()));
+        }
+
+        result
+    }
+}
+
+fn load_dump<'a>() -> Vec<Article> {
+    let raw = fs::read_to_string("namuwiki_202103012.json").unwrap();
+    let s: serde_json::Value = serde_json::from_str(&raw).unwrap();
+
+    s.as_array()
+        .unwrap()
+        .iter()
+        .map(|x| Article::from(x))
+        .collect::<Vec<Article>>()
 }
 
 fn main() {
-    let fs = fs::read_to_string("namuwiki_202103012.json").unwrap();
-    let js: serde_json::Value = serde_json::from_str(&fs).unwrap();
+    let js = load_dump();
 
-    for x in js.as_array().unwrap() {
-        if x["title"].as_str().unwrap().starts_with(&"환상향") {
-            println!("{:#?}", get_classes(x["text"].as_str().unwrap()));
+    let mut r: Vec<(&str, Vec<&str>)> = Vec::new();
+
+    for x in &js {
+        let class = x.classes();
+
+        // if x["title"].as_str().unwrap().contains(&"제로부터 시작하는") {
+        //     println!("{:#?}", x);
+        // }
+
+        // if class.contains(&"일본 애니메이션/목록") {
+        //     println!("{}", x["title"].as_str().unwrap());
+        // }
+
+        if class.iter().any(|x| x.starts_with("일본 애니메이션/")) {
+            // println!("{}", x["title"].as_str().unwrap());
+            r.push((&x.title, class));
         }
     }
+
+    r.sort_by(|a, b| a.0.cmp(b.0));
+
+    fs::write("t.json", format!("{:#?}", r)).unwrap();
 }
