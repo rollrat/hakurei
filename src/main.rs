4e159d839c3e0405035365841f39d49ebd3d7a3f
rollrat <rollrat.cse@gmail.com>
2023-03-15 14:17:09 +0900

    feat(json): implements split json file method for using simd-json

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    split_json_file();
+}
+
+fn split_json_file() {
+    let fs = fs::read_to_string("namuwiki_202103012.json").unwrap();
+    let json: serde_json::Value = serde_json::from_str(&fs).unwrap();
+    let arr = json.as_array().unwrap();
+
+    let length = arr.len();
+
+    let mut r1: Vec<&serde_json::Value> = Vec::new();
+    let mut r2: Vec<&serde_json::Value> = Vec::new();
+    let mut r3: Vec<&serde_json::Value> = Vec::new();
+
+    for i in 0..length {
+        let x = match i % 3 {
+            0 => &mut r1,
+            1 => &mut r2,
+            2 => &mut r3,
+            _ => panic!(),
+        }
+        .push(&arr[i]);
+    }
+
+    fs::write("0.json", &serde_json::to_string(&r1).unwrap()).unwrap();
+    fs::write("1.json", &serde_json::to_string(&r2).unwrap()).unwrap();
+    fs::write("2.json", &serde_json::to_string(&r3).unwrap()).unwrap();
 }
