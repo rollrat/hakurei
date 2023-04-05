6d2a45e1cb7d1a9a36e3e4f6563de4e38a46ed30
rollrat <rollrat.cse@gmail.com>
2023-04-05 13:36:41 +0900

    feat(title): clone file struct for remove mutable borrow

--- src/index/title.rs
+++ src/index/title.rs
@@ -32,7 +32,7 @@ impl TitleIndex {
         }
     }
 
-    pub fn get_no_redirect(&mut self, key: &str) -> Option<Article> {
+    pub fn get_no_redirect(&self, key: &str) -> Option<Article> {
         if !self.map.contains_key(key) {
             return None;
         }
@@ -42,19 +42,18 @@ impl TitleIndex {
         let offset_start = index[0];
         let offset_end = index[1];
 
-        self.file
-            .seek(SeekFrom::Start(offset_start as u64))
-            .unwrap();
+        let mut file = self.file.try_clone().unwrap();
+        file.seek(SeekFrom::Start(offset_start as u64)).unwrap();
 
         let mut buf = vec![0; offset_end - offset_start + 1];
-        self.file.read(&mut buf).unwrap();
+        file.read(&mut buf).unwrap();
 
         let raw = String::from_utf8(buf).unwrap();
 
         Some(serde_json::from_str(&raw).unwrap())
     }
 
-    pub fn get(&mut self, key: &str) -> Option<Article> {
+    pub fn get(&self, key: &str) -> Option<Article> {
         if !self.map.contains_key(key) {
             return None;
         }
